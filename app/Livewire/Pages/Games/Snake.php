16fca7cc92ef5ba60ed347db2eff30f4d278b9cb
bearjcc <bearjcc@gmail.com>
2025-10-13 15:57:35 +1300

    feat(games): create Livewire components for all games
    Add component classes for:
    - Connect4
    - Minesweeper
    - Snake
    - TwentyFortyEight
    
    All games now have proper Livewire routing.

---
+++ app/Livewire/Pages/Games/Snake.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages\Games;
+
+use Livewire\Component;
+
+class Snake extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.games.snake');
+    }
+}
